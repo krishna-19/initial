5afac55b8ebfbb72848a0fe64f704d6cc4b99710
krishna-19 <krishnacool5051@gmail.com>
2020-06-01 12:43:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var electronics = {};
+module["laptop"] = electronics;
+electronics.brand = require("./brand");
+electronics.model =("./model");
+electronics.os = ("./os");
+electronics.price = ("./price");
